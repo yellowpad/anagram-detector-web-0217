64b9a04336127963938da2650b6ea53575c52062
Zayne Abraham <zayneabraham1@gmail.com>
2017-02-09 18:10:36 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    temp_array = []
+    words.each do |wordcheck|
+      if (wordcheck.downcase.split(//).sort == word.downcase.split(//).sort)
+        temp_array.push(wordcheck)
+      end
+    end
+    return temp_array
+  end
+end
